c5e662292834643e5e299214082d511fbdde6dde
yiiiiilin <yilinliu7@gmail.com>
2022-04-19 14:45:15 -0500

    Create server

---
+++ server/index.js
@@ -0,0 +1,9 @@
+//require('dotenv').config();
+const express = require('express');
+
+const app = express();
+app.use(express.json());
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log('server is running on port 3000~~~~~')
+})
