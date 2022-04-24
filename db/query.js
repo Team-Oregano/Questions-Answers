515ed6d59d8f9ebd0bc7e21b36a85795a7e8bb92
yiiiiilin <yilinliu7@gmail.com>
2022-04-24 00:10:25 -0500

    Finish all routes

--- db/query.js
+++ db/query.js
@@ -1,9 +1,8 @@
 const pool = require('./db.js');
 
-const getQuestions = (req, res) => {
+const getQuestions = async (req, res) => {
   const { product_id, page, count } = req.query;
-  const offset = page * count;
-  pool.query(`SELECT row_to_json(quest)
+  await pool.query(`SELECT row_to_json(quest)
   from(
     select product_id,
     (
@@ -39,78 +38,52 @@ const getQuestions = (req, res) => {
   });
 };
 
-// const getQuestions = (req, res) => {
-//   const { product_id, page, count } = req.query;
-//   const offset = page * count;
-//   pool.query(`SELECT questions.id AS question_id, questions.body, questions.date_written, questions.asker_name, questions.helpful, questions.reported,
-//   jsonb_agg(jsonb_build_object(
-//     'answers', answers.body)) answers
-//   FROM questions
-//   LEFT OUTER JOIN answers ON questions.id = answers.question_id
-//   WHERE product_id = $1
-//   GROUP BY questions.id
-//   ORDER BY helpful DESC
-//   LIMIT $2
-//   OFFSET $3`, [product_id, count, offset], (err, results) => {
-//     if (err) {
-//       console.log(err);
-//     } else {
-//       res.send(results.rows);
-//     }
-//   });
-// };
-
-const getAnswers = (req, res) => {
+const getAnswers = async (req, res) => {
   const { question_id, page, count } = req.query;
-  const offset = page * count;
-  pool.query(
-    `SELECT * FROM answers
-  WHERE question_id = $1
-  ORDER BY helpfulness DESC
-  LIMIT $2
-  OFFSET $3`,
-    [question_id, count, offset],
+  await pool.query(
+    `
+SELECT json_build_object(
+    'question', id,
+    'page', ${page},
+    'count', ${count},
+    'results',
+    (
+      select json_agg(
+        json_build_object(
+          'answer_id', id,
+          'body', body,
+          'date', data_written,
+          'answerer_name', answerer_name,
+          'helpfulness', helpful,
+          'photos',
+          (select json_agg(
+            json_build_object(
+              'id', id,
+              'url', url
+            )
+          ) from answers_photos
+          where answer_id = answers.id
+        )
+      )
+    ) as results
+    from answers
+    where question_id = questions.id
+    )
+  ) from questions where id = $1 `,
+    [question_id],
     (err, results) => {
       if (err) {
         res.send(err);
       } else {
-        res.send({ question_id, results: results.rows });
+        res.send(results.rows);
       }
     },
   );
 };
 
-//   Select answers.id AS answer_id, answers.body, answers.data_written, answers.answerer_name, answers.answerer_email, answers.reported, answers.helpful
-//   jsonb_agg(jsonb_build_object(
-//     'answer_photos', answer_photos.url)) answer_photos
-//   FROM answers
-//   LEFT OUTER JOIN answers ON answers.id = answer_photos.answer_id
-//   WHERE question_id = $1
-//   GROUP BY answers.id
-//   ORDER BY helpful DESC
-//   LIMIT $2
-//   OFFSET $3
-
-// const getAnswers = (id, page = 0, count = 5, cb) => {
-//   page = page - 1;
-//   let offset = page * count;
-//   client.query(
-//   `SELECT * FROM answers_format
-//   WHERE questionid = $1
-//   ORDER BY helpfulness DESC
-//   LIMIT $2
-//   OFFSET $3`, [id, count, offset], (err, results) => {
-//     if (err) {
-//       cb(err, null);
-//     } else {
-//       cb(null, results.rows);
-//     }
-//   })
-// }
-
-const markQuestionHelpful = (req, res) => {
-  const { id } = req.body;
-  pool.query('UPDATE questions SET question_helpfulness = question_helpfulness + 1 WHERE question_id = $1', [id], (err, results) => {
+const markQuestionHelpful = async (req, res) => {
+  const { question_id } = req.params;
+  await pool.query('UPDATE questions SET helpful = helpful + 1 WHERE id = $1', [question_id], (err, results) => {
     if (err) {
       console.log(err);
     } else {
@@ -119,9 +92,9 @@ const markQuestionHelpful = (req, res) => {
   });
 };
 
-const markAnswerHelpful = (req, res) => {
-  const { id } = req.body;
-  pool.query('UPDATE answers_format SET helpfulness = helpfulness + 1 WHERE answer_id = $1', [id], (err, results) => {
+const markAnswerHelpful = async (req, res) => {
+  const { answer_id } = req.params;
+  await pool.query('UPDATE answers SET helpful = helpful + 1 WHERE id = $1', [answer_id], (err, results) => {
     if (err) {
       console.log(err);
     } else {
@@ -130,10 +103,93 @@ const markAnswerHelpful = (req, res) => {
   });
 };
 
+const reportQuestion = async (req, res) => {
+  const { question_id } = req.params;
+  await pool.query('UPDATE questions SET reported = true WHERE id = $1', [question_id], (err, results) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send('Question reported');
+    }
+  });
+};
+
+const reportAnswer = async (req, res) => {
+  const { answer_id } = req.params;
+  await pool.query('UPDATE answers SET reported = true WHERE id = $1', [answer_id], (err, results) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send('Answer reported');
+    }
+  });
+};
+
+const addPhotos = async (answer_id, newAnswer) => {
+  await newAnswer.photos.forEach((photo) => {
+    [answer_id].push(photo);
+    return pool.query('INSERT INTO answers_photos (answer_id, url) VALUES($1, $2)', [answer_id]);
+  });
+};
+
+const addQuestion = async (req, res) => {
+  const values = [
+    req.body.product_id, req.body.body, new Date().getTime(), req.body.asker_name, req.body.asker_email,
+  ];
+  await pool.query(
+    'INSERT INTO questions(product_id, body, date_written, asker_name, asker_email, reported, helpful) VALUES($1, $2, $3, $4, $5, false, 0)',
+    values,
+    (err, results) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.send('Question added');
+      }
+    },
+  );
+};
+
+const addAnswer = async (req, res) => {
+  const values = [
+    req.body.question_id, req.body.body, new Date().getTime(), req.body.answerer_name, req.body.answerer_email,
+  ];
+  await pool.query(
+    'INSERT INTO answers(question_id, body, data_written, answerer_name, answerer_email, reported, helpful) VALUES($1, $2, $3, $4, $5, false, 0) RETURNING id',
+    values,
+
+    // (err, results) => {
+    //   if (err) {
+    //     res.send(err);
+    //   } else {
+    //     res.send('Answer added');
+    //   }
+    // },
+  ).then((data) => {
+    if (req.body.photos.length > 0) {
+      const answer_id = Number(data.rows[0].id);
+      addPhotos(answer_id, req.body)
+        .then(() => {
+          res.status(201).send('Photo Added');
+        })
+        .catch((err) => {
+          res.status(400).send('err adding photos');
+        });
+    } else {
+      res.status(200).send('Answer added');
+    }
+  }).catch((err) => {
+    res.status(400).send('err posting answer');
+  });
+};
+
 module.exports = {
   getQuestions,
   getAnswers,
   markAnswerHelpful,
   markQuestionHelpful,
-
+  reportQuestion,
+  reportAnswer,
+  addPhotos,
+  addQuestion,
+  addAnswer,
 };
